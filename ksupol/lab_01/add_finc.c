b3cd7eb51163d11db08a4fa491d1cd4c316855f4
pka17u738 <KsenyaPolyakovaaa@mail.ru>
2019-03-16 15:28:29 +0300

    #3 add function to generate points

--- ksupol/lab_01/add_finc.c
+++ ksupol/lab_01/add_finc.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
+#define OK 0
+#define ERR -1
+
 double f(double x)
 {
 	return sin(x + 90);
@@ -8,16 +11,44 @@ double f(double x)
 
 int main(int argc, char **argv)
 {
+	if (argc < 2)
+	{
+		printf("Not enough data!\n");
+		return ERR;
+	}
 	setbuf(stdout, NULL);
 	int am;
+	int rc;
 	double from;
 	double to;
 	double step;
-	printf("Enter from and to and step: ");
-	scanf("%lf %lf %lf", &from, &to, &step);
+	printf("Enter point 'from': ");
+	rc = scanf("%lf", &from);
+	if (rc != 0)
+	{
+		printf("Wrong point 'from'!\n");
+		return ERR;
+	}
+	printf("\nEnter point 'to': ");
+	rc = scanf("%lf", &to);
+	if (rc != 0)
+	{
+		printf("Wrong point 'to'!\n");
+		return ERR;
+	}
+	printf("\nEnter step: ");
+	rc = scanf("%lf", &step);
+	if (rc != 0)
+	{
+		printf("Wrong step!\n");
+		return ERR;
+	}
 	FILE *g = fopen(argv[1], "w");
 	if (!g)
-		return -1;
+	{
+		printf("Couldn't open file!\n");
+		return ERR;
+	}
 	am = (to - from) / step + 1;
 	fprintf(g, "%d\n", am);
 	for (double i = from; i <= to; i += step)
